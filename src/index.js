eacbb7e44545e6a4e81d451ac997744d0d358cc3
Italo <italo889@gmail.com>
2021-08-09 03:35:02 -0300

    Projeto calculadora finalizado

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Calculator from './main/Calculator'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Calculadora</h1>
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
